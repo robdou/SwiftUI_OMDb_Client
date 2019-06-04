9a1775b826fd76407d0bb9b58d936073c0eb9be7
Robin Douglas <robin.1996@live.co.uk>
2019-06-04 14:08:07 +0100

    Initial commit

---
+++ UISwift_OMDb_Client/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  UISwift_OMDb_Client
+//
+//  Created by Robin Douglas on 04/06/2019.
+//  Copyright © 2019 Robin Douglas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
